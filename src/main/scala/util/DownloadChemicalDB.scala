f175aa42e5ac8df9cf287dd4e36f44b484a2c2d5
Robert Liu <robert910331@sina.com>
2016-10-11 23:14:16 -0400

    seperate the code of DownloadChemicalDB.scala

--- src/main/scala/util/DownloadChemicalDB.scala
+++ src/main/scala/util/DownloadChemicalDB.scala
@@ -5,13 +5,11 @@ import java.io.PrintWriter
 import adapter.AdapterOptions
 import com.mongodb.{BasicDBList, MongoClient}
 
-import scala.io.Source
-
 /**
-  * Created by Robert on 2016/9/22.
+  * Created by Robert on 2016/10/11.
   */
-//  mvn exec:java -Dexec.mainClass="util.ObjectVisualization" -Dexec.args="--inputDB predsynth --input-collection annotated_papers --port-num 27017 --port-name localhost"
-object ObjectVisualization {
+//  mvn exec:java -Dexec.mainClass="util.ObjectVisualization" -Dexec.args="--inputDB predsynth --input-collection chemical_db --port-num 27017 --port-name localhost"
+class DownloadChemicalDB {
     def main(args: Array[String]): Unit = {
         val opts = new AdapterOptions
         opts.parse(args)
@@ -24,23 +22,17 @@ object ObjectVisualization {
         val inputDB = mongo.getDB(opts.inputDB.value)
         val inputCollection = inputDB.getCollection(opts.inputCollection.value)
 
-        val writer = new PrintWriter("../annotated_papers", "UTF-8")
+        val writer = new PrintWriter("../chemical_db.txt", "UTF-8")
         val cursor = inputCollection.find
         while (cursor.hasNext) {
             val next = cursor.next.toMap
-            writer.write(next + "\n")
+            writer.write(next.get("_id") + "\n")
+            val names = next.get("names").asInstanceOf[BasicDBList].toArray
+            names.foreach(name => writer.write(name + "\n"))
+            writer.write("\n")
         }
         writer.flush()
         writer.close()
         mongo.close()
-    }
-}
-
-object NewLineReplacing{
-    def main(args: Array[String]): Unit = {
-        val writer = new PrintWriter("../chemical_db_new.txt", "UTF-8")
-        Source.fromFile("../chemical_db.txt").getLines.foreach(l => writer.write(l + "\n"))
-        writer.flush()
-        writer.close()
     }
 }
